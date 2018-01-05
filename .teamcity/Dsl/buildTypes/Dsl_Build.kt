cbc7331d03d1a914aa0e69ad96ce8e03d4378c52
Yahor <Yahor@tut.by>
2018-01-05 11:56:26 +0300

    added python trp

--- .teamcity/Dsl/buildTypes/Dsl_Build.kt
+++ .teamcity/Dsl/buildTypes/Dsl_Build.kt
@@ -8,12 +8,15 @@ object Dsl_Build : BuildType({
     uuid = "7f979d2a-d5d4-41fe-9927-5316d28080d1"
     id = "Dsl_Build"
     name = "Build"
-    val the_list = listOf("Java 1.7", "Java 1.8", "Gradle 3", "Gradle 4", "Terraform 3", "Terraform 4", "Python 5")
 
     params {
         select("Choice", "", label = "Choose necessary software", display = ParameterDisplay.PROMPT,
                 allowMultiple = true,
-                options = listOf(the_list)
+                options = listOf(
+                        "Java 1.7", "Java 1.8",
+                        "Gradle 3", "Gradle 4",
+                        "Terraform 3", "Terraform 4",
+                        "Python 3"))
         select("Installed", "in", label = "Installed software", display = ParameterDisplay.PROMPT,
                 options = listOf("Java 1.7", "Gradle 3", "Gradle 2"))
     }
