2dff36ea3f894b838c3d38f3bfe61d4ca32111d5
egormin <egormin@TeamCity>
2018-01-05 12:12:08 +0300

    TeamCity change in 'DSL' project: parameters of 'Build' build configuration were updated

--- .teamcity/Dsl/patches/buildTypes/7f979d2a-d5d4-41fe-9927-5316d28080d1.kts
+++ .teamcity/Dsl/patches/buildTypes/7f979d2a-d5d4-41fe-9927-5316d28080d1.kts
@@ -18,8 +18,8 @@ changeBuildType("7f979d2a-d5d4-41fe-9927-5316d28080d1") {
                       checked = "true")
         }
         add {
-            checkbox("three", "three", label = "three", display = ParameterDisplay.PROMPT,
-                      checked = "true", unchecked = "false")
+            checkbox("three", "three", label = "three", display = ParameterDisplay.PROMPT, readOnly = true,
+                      checked = "false", unchecked = "true")
         }
         add {
             checkbox("two", "two", label = "two", display = ParameterDisplay.PROMPT,
